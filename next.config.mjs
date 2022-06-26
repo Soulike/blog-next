0fe27e41b7531b1e79ec602e722aa46779d386b6
Soulike <soulike@qq.com>
2022-06-26 12:09:14 +0800

    feat: add proxy for dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: "build",
+    reactStrictMode: true,
+    distDir: 'build',
+    async rewrites() {
+        return process.env.NODE_ENV === 'development'
+            ? [
+                  {
+                      source: '/server/:path*',
+                      destination: 'https://soulike.tech/server/:path*',
+                  },
+              ]
+            : [];
+    },
 };
 
 export default nextConfig;
