d1a0d26401191750c464bf9e9a8154251a4e1a4e
Soulike <soulike@qq.com>
2022-06-26 13:53:35 +0800

    feat: add /about

---
+++ src/components/About/Container.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+
+import {Option} from '@/src/apis';
+
+import {AboutView} from './View';
+
+export function About() {
+    const [about, setAbout] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const getAbout = async () =>
+        {
+            const result = await Option.get();
+            return result === null ? '' : result.about;
+        };
+
+        setLoading(true);
+        getAbout()
+            .then((about) => {
+                setAbout(about);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    return <AboutView about={about} loading={loading} />;
+}
--- src/components/ArticleShower/index.ts
+++ src/components/ArticleShower/index.ts
@@ -1,1 +1,4 @@
-export * from './Container';
+import dynamic from 'next/dynamic';
+const promise = import('./Container').then(({ArticleShower}) => ArticleShower);
+
+export const ArticleShower = dynamic(() => promise, {ssr: false});
