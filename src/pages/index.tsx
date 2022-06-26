26182afe92de67d43e9d259a55baa789e08a85ce
Soulike <soulike@qq.com>
2022-06-26 14:18:55 +0800

    feat: use dynamic import for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
-import {Index} from '@/src/components/Index';
+import dynamic from 'next/dynamic';
+
+const IndexPromise = import('@/src/components/Index').then(({Index}) => Index);
+
+const Index = dynamic(() => IndexPromise, {ssr: false});
 
 export default function IndexPage() {
     return <Index />;
