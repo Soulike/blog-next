d4d578fd6674e3c7f5455cc9ed089ba29e3cb2e9
Soulike <soulike@qq.com>
2022-06-27 07:49:43 +0800

    fix: add redirect in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import dynamic from 'next/dynamic';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 
 const IndexPromise = import('@/src/components/Index').then(({Index}) => Index);
 
 const Index = dynamic(() => IndexPromise, {ssr: false});
 
 export default function IndexPage() {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (window.location.pathname !== router.pathname) {
+            router.replace(`/${window.location.pathname}`);
+        }
+    }, [router]);
+
     return <Index />;
 }
