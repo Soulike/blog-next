b73e9f61e5e1c963ab31a58f58d95e10ed19b7fa
Soulike <soulike@qq.com>
2022-07-02 14:05:25 +0800

    refactor: add and apply useCategories hook

--- src/components/Frame/View.tsx
+++ src/components/Frame/View.tsx
@@ -13,6 +13,7 @@ import avatar from '@/src/static/avatar.png';
 import {Category} from '@/src/types';
 import {getCategoryLink} from '@/src/utils/route';
 
+import {Loading} from '../Loading';
 import Style from './Style.module.scss';
 
 const {Sider, Footer, Content, Header} = Layout;
@@ -20,12 +21,13 @@ const {Sider, Footer, Content, Header} = Layout;
 interface Props {
     hitokoto: string;
     year: number;
-    categoryList: Array<Category>;
+    categories: Array<Category>;
+    loading: boolean;
     children?: React.ReactNode;
 }
 
 export function FrameView(props: Props) {
-    const {hitokoto, year, categoryList, children} = props;
+    const {hitokoto, year, categories, children, loading} = props;
     const menuItems = [
         {
             label: (
@@ -48,7 +50,7 @@ export function FrameView(props: Props) {
                 </>
             ),
             key: 'categorySubmenu',
-            children: categoryList.map((category) => {
+            children: categories.map((category) => {
                 const {id, name} = category;
                 return {
                     label: (
@@ -82,53 +84,58 @@ export function FrameView(props: Props) {
 
     return (
         <Layout className={Style.Frame}>
-            {/*在宽 500px 以上屏幕显示的 Sider*/}
-            <Sider theme={'light'} className={Style.sidebar}>
-                <div className={Style.sidebarInner}>
-                    <div className={Style.avatarWrapper}>
-                        <img
-                            src={avatar.src}
-                            className={Style.avatar}
-                            alt={'avatar'}
-                        />
-                    </div>
-                    <Menu
-                        className={Style.menu}
-                        mode={'inline'}
-                        selectable={false}
-                        items={menuItems}
-                    />
-                </div>
-            </Sider>
-
-            <Layout className={Style.main}>
-                {/*在宽 500 px 以下屏幕显示的 Header*/}
-                <Header className={Style.header}>
-                    <div className={Style.headerInner}>
-                        <div className={Style.avatarWrapper}>
-                            <img
-                                src={avatar.src}
-                                className={Style.avatar}
-                                alt={'avatar'}
+            {loading && <Loading />}
+            {!loading && (
+                <>
+                    {/*在宽 500px 以上屏幕显示的 Sider*/}
+                    <Sider theme={'light'} className={Style.sidebar}>
+                        <div className={Style.sidebarInner}>
+                            <div className={Style.avatarWrapper}>
+                                <img
+                                    src={avatar.src}
+                                    className={Style.avatar}
+                                    alt={'avatar'}
+                                />
+                            </div>
+                            <Menu
+                                className={Style.menu}
+                                mode={'inline'}
+                                selectable={false}
+                                items={menuItems}
                             />
                         </div>
-                        <Menu
-                            className={Style.menu}
-                            mode={'horizontal'}
-                            selectable={false}
-                            items={menuItems}
-                            theme={'light'}
-                        />
-                    </div>
-                </Header>
-                <Content className={Style.content}>{children}</Content>
-                <Footer className={Style.footer}>
-                    <div className={Style.info}>
-                        {year} - Designed & Created by Soulike
-                    </div>
-                    <div className={Style.hitokoto}>{hitokoto}</div>
-                </Footer>
-            </Layout>
+                    </Sider>
+
+                    <Layout className={Style.main}>
+                        {/*在宽 500 px 以下屏幕显示的 Header*/}
+                        <Header className={Style.header}>
+                            <div className={Style.headerInner}>
+                                <div className={Style.avatarWrapper}>
+                                    <img
+                                        src={avatar.src}
+                                        className={Style.avatar}
+                                        alt={'avatar'}
+                                    />
+                                </div>
+                                <Menu
+                                    className={Style.menu}
+                                    mode={'horizontal'}
+                                    selectable={false}
+                                    items={menuItems}
+                                    theme={'light'}
+                                />
+                            </div>
+                        </Header>
+                        <Content className={Style.content}>{children}</Content>
+                        <Footer className={Style.footer}>
+                            <div className={Style.info}>
+                                {year} - Designed & Created by Soulike
+                            </div>
+                            <div className={Style.hitokoto}>{hitokoto}</div>
+                        </Footer>
+                    </Layout>
+                </>
+            )}
         </Layout>
     );
 }
---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react';
+
+import {Category as CategoryApi} from '@/src/apis';
+import {Category} from '@/src/types';
+
+export function useCategories(): {
+    loading: boolean;
+    categories: Category[] | null;
+} {
+    const [categories, setCategories] = useState<Category[] | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        setCategories(null);
+        CategoryApi.getAll()
+            .then((categories) => setCategories(categories))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return {loading, categories};
+}
