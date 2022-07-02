4a332c23b3fb1e2f5fc96c31f4b5401ba1ad09d8
Soulike <soulike@qq.com>
2022-07-02 11:24:26 +0800

    feat: add useArticle hook

---
+++ src/hooks/useArticle.tsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from 'react';
+
+import {Article as ArticleApi} from '@/src/apis';
+import {Article} from '@/src/types';
+
+export function useArticle(id: number): {
+    loading: boolean;
+    article: Article | null;
+} {
+    const [loading, setLoading] = useState(true);
+    const [article, setArticle] = useState<Article | null>(null);
+
+    useEffect(() => {
+        setArticle(null);
+        setLoading(true);
+        ArticleApi.getById(id)
+            .then((article) => setArticle(article))
+            .finally(() => setLoading(false));
+    }, [id]);
+
+    return {loading, article};
+}
